23028cc0b50d1bc4d4c8196511c62a449669b01d
Jeroen Rosenberg <jeroen.rosenberg@gmail.com>
2016-03-04 22:27:44 +0100

    Streaming insert projects using websocket connection

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,13 +2,28 @@ package controllers
 
 import javax.inject.Inject
 
-import models.ProjectRepo
+import akka.stream.scaladsl.Flow
+import models.{Project, ProjectRepo}
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.libs.json.Json
-import play.api.mvc.{Action, Controller}
+import play.api.libs.json.{JsSuccess, Json}
+import play.api.mvc._
+import concurrent.duration._
 
 class Application @Inject()(projectRepo: ProjectRepo)
                            extends Controller {
+
+  implicit val mt = play.api.mvc.WebSocket.MessageFlowTransformer.stringMessageFlowTransformer
+
+  def socket = WebSocket.accept[String, String] { request =>
+    Flow[String]
+      .map(Json.parse)
+      .map(js => Json.fromJson[Project](js))
+      .collect { case JsSuccess(project, _) => project }
+      .groupedWithin(10, 10 millis)
+      .map(_.toList)
+      .mapAsyncUnordered(parallelism = 4)(projectRepo.insert)
+      .map(_.n.toString)
+  }
 
   def createProject(name: String)= Action.async { implicit rs =>
     projectRepo.create(name)
